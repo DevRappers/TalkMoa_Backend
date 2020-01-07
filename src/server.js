9b2fe0455ca1344275396dc55d7d81c7c6c56711
DevRappers <devrappers@gmail.com>
2020-01-07 10:08:47 +0900

    서버 기본구조 정의

--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
+import './env';
 
 require('dotenv').config();
 const PORT = process.env.PORT || 4000;
+
+//라우팅 모듈 선언
+const indexRouter = require('./routes/index');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger('dev'));
+//request 요청 URL과 처리 로직을 선언한 라우팅 모듈 매핑
+app.use('/', indexRouter);
 
 app.listen(PORT, () => console.log(`TalkMoa Server Running 🏃 http://localhost:${PORT}`));
